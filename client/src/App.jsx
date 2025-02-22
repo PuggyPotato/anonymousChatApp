08aefd961851adae423a7c5332fd4e0d8134e98a
PuggyPotato <qingyaurou@gmail.com>
2025-02-22 14:53:33 +0800

    added the input space and send button frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ function App(){
 
   const socket = useRef(null);
 
-  socket.current = io("http://127.0.0.1:5000")
+  //socket.current = io("http://127.0.0.1:5000")
 
   socket.current.on("connected",(message) =>{
     console.log(message)
@@ -14,8 +14,12 @@ function App(){
 
   return(
     <>
-      <div className="border-2">
-        
+      <div className=" h-screen flex justify-center items-center">
+        <form className="border-2 w-128 h-152 relative">
+            <div className="w-full absolute border-2 bottom-16"></div>
+            <input className="absolute bottom-2 w-[85%] left-2 h-12 rounded-2xl border-2 p-2"/>
+            <button className="border-2 absolute bottom-2  right-3  size-12 rounded-full"></button>
+        </form>
       </div>
     </>
   )
